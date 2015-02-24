271e614ca0408ad4f277146215879c63e7f750a0
linkitapps <master@linkit.kr>
2015-02-24 23:35:26 +0900

    제주도로 이동

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -22,20 +22,33 @@ function MainCtrl() {
     selectionIndicator: false,
     navigationHelpButton: false,
     navigationInstructionsInitiallyVisible: false,
+    // ArcGIS imagery provider 를 사용하지만 우리나라 지역의 경우 브이월드 지도를 사용하도록 커스트마이징
     imageryProvider : new Cesium.ArcGisMapServerAndVworldSatelliteImageryProvider({
       url: '//services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
     })
   });
 
+  // 브이월드 hybrid 레이어 추가
   var layers = viewer.scene.imageryLayers;
   layers.addImageryProvider(new Cesium.ArcGisMapServerAndVworldHybridImageryProvider({
     url: '//services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
   }));
 
+  // terrain 설정
   var terrainProvider = new Cesium.CesiumTerrainProvider({
     url : '//cesiumjs.org/tilesets/terrain/smallterrain'
   });
   viewer.terrainProvider = terrainProvider;
+
+  // 제주도로 이동
+  viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(126.5235, 33.3218, 10000.0),
+    orientation : {
+      heading : Cesium.Math.toRadians(0.0),
+      pitch : Cesium.Math.toRadians(-30.0),
+      roll : 0.0
+    }
+  });
 
 }
 

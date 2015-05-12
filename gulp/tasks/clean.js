ee8fd52a3be1269d4a7ab2e7783a90c441a8f488
linkitapps <master@linkit.kr>
2015-05-12 10:44:32 +0900

    gulp dev 실행 후 생기는 build 디렉토리에 app 디렉토리에 있는 Cesium 디렉토리를 수동으로 복사해주어야 했던 것을 gulp clean task 의 내용을 수정해서 clean 해도 Cesium 디렉토리가 삭제되지 않도록 변경함. 최초에 build 디렉토리가 생기면 한번만 복사해서 넣어주면 됨.

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -6,6 +6,7 @@ var del    = require('del');
 
 gulp.task('clean', function(cb) {
 
-  del([config.dist.root], cb);
+  // Cesium 라이브러리 디렉토리는 /app 에서 복사해온 것인데 build 할 때마다 삭제 되는 것을 방지해야 해서 아래와 같이 처리
+  del([config.dist.root + '/*', '!' + config.dist.root + '/Cesium'], cb);
 
 });
